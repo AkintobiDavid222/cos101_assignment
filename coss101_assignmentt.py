14bf38103d1cd58b009dd9d3d970969c97f326bc
AkintobiDavid222 <david.akintobi@binghamuni.edu.ng>
2024-12-27 15:56:36 +0100

    Add files via upload

---
+++ coss101_assignmentt.py
@@ -0,0 +1,20 @@
+spanish_to_english = {
+    "Hola": "Hello",
+    "Adiós": "Goodbye",
+    "Gracias": "Thank you",
+    "Por favor": "Please",
+    "Amor": "Love",sssss
+    "Feliz": "Happy",
+    "Casa": "House",
+    "Libro": "Book",
+    "Familia": "Family",
+    "Amigo": "Friend",
+    "Escuela": "School",
+    "Comida": "Food",
+    "Agua": "Water",
+    "Tiempo": "Time",
+    "Noche": "Night"
+}
+
+# Example usage
+print(spanish_to_english["Hola"])  # Output: Hello
